084b0ad2af9e329304991fb1739429270abc8545
euglazer <eugenestapler@gmail.com>
2016-02-14 21:18:00 -0800

    remove old omniauth jtoker scar tissue

--- js/interface/auth.js
+++ js/interface/auth.js
@@ -1,19 +1,10 @@
 $.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-      // append outbound auth headers
-      $.auth.appendAuthHeaders(xhr, settings);
-    }
+  beforeSend: function(xhr, settings) {
+    // append outbound auth headers
+    $.auth.appendAuthHeaders(xhr, settings);
+  }
 });
 
-// $.auth.configure({
-//   apiUrl: 'http://104.236.175.6'
-// });
-
-
 $.auth.configure({
-  apiUrl: 'http://104.236.175.6',
-  forceHardRedirect: true,
-  authProviderPaths: {
-    facebook: '/auth/facebook' // <-- note that this is different than what was set with github
-  }
+  apiUrl: 'http://104.236.175.6'
 });
