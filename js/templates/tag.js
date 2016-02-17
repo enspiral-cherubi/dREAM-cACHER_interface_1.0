3d65f22145050669246ebcd68c816c1ecdc0f282
euglazer <eugenestapler@gmail.com>
2016-02-16 21:07:03 -0800

    refactor tag template

--- js/templates/tag.js
+++ js/templates/tag.js
@@ -1,5 +1,6 @@
 module.exports = function (text) {
-  return "<a " + "href='/" + text + "' " + "class='tag' " + "id='" + text + "'>"
-       + text
-       + "</a>"
+  return ""
+    + "<a href='/" + text + "' class='tag' id='" + text + "'>"
+    +   text
+    + "</a>"
 }
